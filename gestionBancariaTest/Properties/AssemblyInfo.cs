b4936e7f73516049c3964e073f0474fd5ccf9437
Kure2323 <polgorrindo@gmail.com>
2025-01-17 20:14:43 +0100

    Sin empezar el punto número 5

---
+++ gestionBancariaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gestionBancariaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gestionBancariaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00b7769a-2ac2-454f-8855-31a05899d376")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ gestionBancariaTest/gestionBancariaTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using gestionBancariaApp;
+namespace gestionBancariaTest
+{
+    [TestClass]
+
+    public class gestionBancariaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
